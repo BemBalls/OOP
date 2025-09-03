a205cbd32e5d851e078af4afe043d77add09316d
BemBalls <11820033@usc.edu.ph>
2025-09-03 23:41:23 +0800

    Add files via upload

---
+++ Rainbow.java
@@ -0,0 +1,15 @@
+public class Rainbow {
+    
+    public static void main(String args[]) 
+    {
+        boolean sunny = true;
+        boolean rainy = false; 
+        
+        if(isSunny == true && isRainy == true)
+        {
+            System.out.println("Tadahh! There's a rainbow!");
+        } else {
+            System.out.println("Oh Noo! There's no rainbow!");
+        }
+    }
+}
